55c7158e79d800c702df2205ec48be937d7888db
mkerin <m4tt.kerin@gmail.com>
2019-01-25 15:34:27 +0000

    Added local version of Liu method with fixes to possible bugs

--- struct_lmm/utils/__init__.py
+++ struct_lmm/utils/__init__.py
@@ -3,10 +3,12 @@ r"""
 - :func:`.import_one_pheno_from_csv`
 - :func:`.norm_env_matrix`
 - :func:`.make_out_dir`
+- :func:`.mod_liu_corrected`
 """
 
 from .fdr import fdr_bh
 from .sugar_utils import import_one_pheno_from_csv, make_out_dir, norm_env_matrix
+from .chiscore_liu import mod_liu_corrected
 
 __all__ = [
     "fdr",
@@ -14,4 +16,5 @@ __all__ = [
     "norm_env_matrix",
     "make_out_dir",
     "fdr_bh",
+    "mod_liu_corrected"
 ]
